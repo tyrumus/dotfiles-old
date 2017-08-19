c9aa1b32dd5aa36e2b449e0b0dff3e99a9e62ee1
Lego Stax <legostax@gmail.com>
2017-08-18 19:16:17 -0700

    Update music-client.py

--- music-client.py
+++ music-client.py
@@ -10,10 +10,3 @@ s.connect((HOST, PORT))
 s.send(cmd)
 s.close()
 sys.exit(0)
-
-#while 1:
-#    msg = raw_input("> ")
-#    if msg == "close":
-#        s.close()
-#        sys.exit(0)
-#    s.send(msg)
